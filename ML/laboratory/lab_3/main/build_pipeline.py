50da12e202acefecfe95c7a7585d83d93b35e9ae
Sergey Motynga <Motynga12345@mail.ru>
2025-05-29 00:44:37 +0600

    feat: not finished lab_3

--- ML/laboratory/lab_3/main/build_pipeline.py
+++ ML/laboratory/lab_3/main/build_pipeline.py
@@ -1,12 +1,22 @@
+from imblearn.under_sampling import RandomUnderSampler
 from imblearn.combine import SMOTEENN
+from imblearn.over_sampling import SMOTE
 from imblearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 
 
-def build_pipeline(model):
-    pipe = Pipeline([
-        ('scaler', StandardScaler()),
-        ('sampler', SMOTEENN(sampling_strategy=1, random_state=42)),
-        ('model', model)
-    ])
-    return pipe
+def build_pipeline(model, scaler: bool=True, sampler='undersample', sampling_strategy=0.3):
+    steps = []
+
+    if scaler:
+        steps.append(('scaler', StandardScaler()))
+
+    if sampler == 'combine':
+        steps.append(('sampler', SMOTEENN(sampling_strategy=sampling_strategy, random_state=42)))
+    elif sampler == 'undersample':
+        steps.append(('sampler', RandomUnderSampler(sampling_strategy=0.3, random_state=42)))
+    else:
+        steps.append(('sampler', SMOTE(sampling_strategy=0.3, random_state=42)))
+
+    steps.append(('model', model))
+    return Pipeline(steps)
